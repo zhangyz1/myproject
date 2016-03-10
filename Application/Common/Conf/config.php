4c1b5359d665b8985fbbe88ab347579e76f2333d
zhangyongzhao <zhangyongzhao@yunmai365.com>
2016-03-10 18:05:07 +0800

    [ceshi]xiangmu

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	// 视图层
+    'DEFAULT_THEME'        => 'Default', // 默认模板主题名称
+    'LAYOUT_ON'            => true, // 是否启用布局
+    'LAYOUT_NAME'          => 'layout', // 当前布局名称 默认为layout
+);
