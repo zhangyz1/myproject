a7c2356b2ca7e29b7182f9e00d7511f54b3ed460
zhangyongzhao <zhangyongzhao@yunmai365.com>
2016-03-22 18:14:26 +0800

    【home】修改错误

--- Application/Home/Controller/ContactController.class.php
+++ Application/Home/Controller/ContactController.class.php
@@ -5,7 +5,7 @@ class ContactController extends Controller {
     public function index(){
         if (IS_POST) {
           $data= I('post.');
-          $user = D('Massage')->insert(array('name'=>$data['name'],'email'=>$data['email'],'content'=>$data['message'],'updated_time'=>time(),'created_time'=>time()));
+          $user = D('Message')->insert(array('name'=>$data['name'],'email'=>$data['email'],'content'=>$data['message'],'updated_time'=>time(),'created_time'=>time()));
           if ($user) {
             echo 'ddddd';
           }
