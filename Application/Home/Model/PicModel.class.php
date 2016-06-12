b76514a373dfa6d1d075c7428e38b53f98a9c29c
hujinzhe <hujinzhe@iyoudoo.com>
2016-06-12 18:09:23 +0800

    model

--- Application/Home/Model/PicModel.class.php
+++ Application/Home/Model/PicModel.class.php
@@ -7,7 +7,7 @@
 // | Author: huajie <banhuajie@163.com>
 // +----------------------------------------------------------------------
 
-namespace Biz\Model;
+namespace Home\Model;
 use Think\Model;
 use Think\Upload;
 
