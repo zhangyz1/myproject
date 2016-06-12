1fa1b958a8de17f309b0288af5752eb96d71fa26
zhangyz1 <jasonaitr@hotmail.com>
2016-06-12 10:26:03 +0800

    Update db.php
    修改密码

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -12,7 +12,7 @@ return array(
     'DB_HOST'       => 'mysql.hostinger.com.hk', // 本机服务器地址
     'DB_NAME'       => 'u314267231_blog',       // 数据库名
     'DB_USER'       => 'u314267231_admin',            // 用户名
-    'DB_PWD'        => 'LGhkL1h4Nv',      // 外网密码
+    'DB_PWD'        => '123456',      // 外网密码
     'DB_PORT'       => '3306',            // 端口
     'DB_PREFIX'     => 'bl_',            // 数据库表前缀
 );
